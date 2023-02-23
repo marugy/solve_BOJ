45664c7e491dc0cb4bdaab0779f2aa5910c716b3
신규람 <gyulife7301@naver.com>
2023-02-23 14:55:28 +0900

    upload : 그래프_BJ_14442_벽부수고이동하기2

---
+++ 그래프/BJ_14442_벽부수고이동하기2.java
@@ -0,0 +1,104 @@
+package BJ;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_14442_벽부수고이동하기2 {
+
+	static int N;
+	static int M;
+	static int K;
+	static char[][] map;
+	static boolean[][][] visited;
+	static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+	static int minDis = Integer.MAX_VALUE;
+	static boolean pass;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 변수들 값 입력 받기
+		N = parseInt(st.nextToken());
+		M = parseInt(st.nextToken());
+		K = parseInt(st.nextToken());
+		
+		//배열 초기화
+		map = new char[N][M];
+		visited = new boolean[N][M][K+1];
+
+		// 맵 입력받기
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+
+		// 경로찾기 실행
+		visited[0][0][0] = true;
+		goMN(0, 0);
+		
+		//경로를 찾지 못했으면 -1
+		if (minDis == Integer.MAX_VALUE)
+			minDis = -1;
+		
+		//결과 출력
+		System.out.println(minDis);
+	}
+
+	public static class Pos { //x 좌표, y 좌표, 움직인 거리, 부수고 온 벽
+		int x;
+		int y;
+		int dis;
+		int broken;
+		public Pos(int x, int y, int dis, int broken) {
+			this.x = x;
+			this.y = y;
+			this.dis = dis;
+			this.broken = broken;
+		}
+	}
+
+	public static void goMN(int x, int y) {
+		Queue<Pos> q = new ArrayDeque<>();
+		q.add(new Pos(x, y, 1, 0));//x:0,y:0,Dis:1,Broken:0 시작
+
+		while (!q.isEmpty()) {
+			Pos now = q.poll();
+			//도착하면 거리 갱신 후 종료
+			if (now.x == N - 1 && now.y == M - 1) {
+				minDis = minDis > now.dis ? now.dis : minDis;
+				return;
+			}
+			//사방탐색
+			for (int i = 0; i < 4; i++) {
+				int xx = now.x + dir[i][0];
+				int yy = now.y + dir[i][1];
+				
+				// 범위를 넘어가면 pass
+				if (!(0 <= xx && xx < N && 0 <= yy && yy < M))
+					continue;
+
+				// 이동할 곳이 벽인 경우
+				if (map[xx][yy] == '1') {
+					// 지금까지 부수고 온 횟수가 K보다 작고 아무도 k번 부수고 방문하지 않았다면??
+					if (now.broken < K && !visited[xx][yy][now.broken + 1]) {
+						visited[xx][yy][now.broken + 1] = true;//한번 더 부수고
+						q.add(new Pos(xx, yy, now.dis + 1, now.broken + 1));//방문
+					}
+				} else {// 벽이 아닌 경우
+					if (!visited[xx][yy][now.broken]) {//지금까지 부순거만큼 방문한 적이 없다면
+						visited[xx][yy][now.broken] = true;
+						q.add(new Pos(xx, yy, now.dis + 1, now.broken));//방문
+					}
+				}
+			}
+		}
+	}
+}
