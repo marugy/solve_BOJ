29e50e3534bb47e5ac3e26d524edbd9fbccbd017
신규람 <gyulife7301@naver.com>
2023-03-08 17:55:44 +0900

    upload : 순열_BJ_15655_N과M_6

---
+++ 순열, 조합/BJ_15655_N과M_6.java
@@ -0,0 +1,54 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_15655_N과M_6 {
+	static int n;
+	static int m;
+	static boolean []visited;
+	static int []num;
+	static int []arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	static void print(int start, int count) {
+		if(count==m) {
+			for(int i=0;i<m;i++)
+				sb.append(num[i]+" ");
+			sb.append("\n");
+			return;
+		}
+		for (int i=start; i<n; i++) {
+			if(!visited[i]) {
+				num[count]=arr[i];
+				visited[i]=true;
+				print(i+1,count+1);
+				visited[i]=false;
+			}
+		}	
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++)
+			arr[i]=Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		visited = new boolean[n];
+		
+		num = new int[m];
+		
+		print(0,0);
+		System.out.println(sb.toString());
+	}
+}
+
