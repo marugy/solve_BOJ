35bb4cccf3171f2315a970ecaf3f6286b078ed0b
ho_0214 <ho_0214@naver.com>
2023-03-22 11:15:00 +0900

    upload : 조합_BJ_2407_조합

---
+++ 순열, 조합/BJ_2407_조합.java
@@ -0,0 +1,27 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BJ_2407_조합 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        BigInteger n1 = BigInteger.ONE;
+        BigInteger n2 = BigInteger.ONE;
+
+        for (int i = 0; i < m; i++) {
+            n1 = n1.multiply(new BigInteger(String.valueOf(n - i)));
+            n2 = n2.multiply(new BigInteger(String.valueOf(i + 1)));
+        }
+        BigInteger answer = n1.divide(n2);
+        System.out.println(answer);
+	}
+}
