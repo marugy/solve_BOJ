2a2a25634b056d7c45c3943339cea9997f7e0d80
Gyuram Shin <91540464+marugy@users.noreply.github.com>
2024-04-15 21:12:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num) {
+        long number = num;
+        int answer = 0;
+        while(number!=1){
+            if(number%2==0) number/=2;
+            else number = number*3+1;
+            answer++;
+            if(answer==500) return -1;
+        }
+        return answer;
+    }
+}
