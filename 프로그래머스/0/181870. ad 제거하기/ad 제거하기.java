58110b29c88d2cdc0d722e9d253c1769c5ebe914
Gyuram Shin <91540464+marugy@users.noreply.github.com>
2024-05-06 21:04:26 +0900

    [level 0] Title: ad 제거하기, Time: 0.85 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String>list = new ArrayList<>();
+        for(String str : strArr){
+            if(str.contains("ad")) continue;
+            list.add(str);
+        }
+        String[] answer = new String[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i);
+        } 
+        
+        return answer;
+    }
+}
