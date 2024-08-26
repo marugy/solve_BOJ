db75eaa90d8b1a93f86919d035fd9367cf31c5b9
Gyuram Shin <91540464+marugy@users.noreply.github.com>
2024-08-27 00:14:16 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i=0;i<array.length; i++){
+            int tmp = array[i];
+            while(tmp>0){
+                if(tmp%10==7) answer++;
+                tmp/=10;
+            }
+        }
+        return answer;
+    }
+}
